6ebfc51ba7fa12a280f631f9dd491b68a058beb8
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-07-10 16:52:51 +0530

    First and Last Position of an Element In Sorted Array

--- DSA_Love_Babbar/9_BinarySearch.cpp
+++ DSA_Love_Babbar/9_BinarySearch.cpp
@@ -1,38 +1,112 @@
+// #include <iostream>
+// using namespace std;
+
+// int Bs(int arr[], int size, int key){
+//     int start = 0;
+//     int end =  size-1;
+
+//     int mid = (start+end)/2;
+
+//     while(start<=end){
+//         if(arr[mid]==key){
+//             return mid;
+//         }
+
+//         //go to right part
+//         if(key>arr[mid]){
+//             start=mid+1;
+//         }else{
+//             //go to left part
+//             end = mid-1;
+//         }
+//         mid = (start+end)/2;
+//     }
+//     return -1;
+// }
+
+// int main(){
+//     int arr[6] = {2,4,6,8,10,12};
+//     int brr[5] = {3,6,9,12,15};
+
+//     int even = Bs(arr, 6, 10);
+//     int odd = Bs(brr, 5, 3);
+    
+//     cout<< "Index of 10 is " << even << endl;
+//     cout<< "Index of 3 is " << odd << endl;
+
+//     return 0;
+// }
+
+
+
+
+
+//First and Last Position of an Element In Sorted Array
+
 #include <iostream>
 using namespace std;
 
-int Bs(int arr[], int size, int key){
+int firstOcc(int arr[], int n, int k){
     int start = 0;
-    int end =  size-1;
+    int end = n-1;
+    int ans=-1;
 
-    int mid = (start+end)/2;
+    int mid = start + (end-start)/2;
 
     while(start<=end){
-        if(arr[mid]==key){
-            return mid;
-        }
-
-        //go to right part
-        if(key>arr[mid]){
-            start=mid+1;
-        }else{
-            //go to left part
+        if(arr[mid]==k){
+            ans = mid;
             end = mid-1;
         }
-        mid = (start+end)/2;
+
+        else if(k>mid){
+            start = mid+1;
+        }
+        
+        else{
+            end = mid-1;
+        }
+        mid = start + (end-start)/2;
     }
-    return -1;
+    return ans;
 }
 
-int main(){
-    int arr[6] = {2,4,6,8,10,12};
-    int brr[5] = {3,6,9,12,15};
 
-    int even = Bs(arr, 6, 10);
-    int odd = Bs(brr, 5, 3);
+int lastOcc(int arr[], int n, int k){
+    int start = 0;
+    int end = n-1;
+    int ans=-1;
+
+    int mid = start + (end-start)/2;
+
+    while(start<=end){
+        if(arr[mid]==k){
+            ans = mid;
+            start = mid+1;
+        }
+
+        else if(k>mid){
+            start = mid+1;
+        }
+        
+        else{
+            end = mid-1;
+        }
+        mid = start + (end-start)/2;
+    }
+    return ans;
+}
+
+ 
+int main(){
+
+    int arr[10] = {1,2,3,3,3,3,4,6,7,9};
     
-    cout<< "Index of 10 is " << even << endl;
-    cout<< "Index of 3 is " << odd << endl;
+    int First = firstOcc(arr,10,3);
+    int Last = lastOcc(arr,10,3);
+
+    cout<<"First occurence of key is "<<First << endl;
+    cout<<"Last occurence of key is "<<Last << endl;
 
     return 0;
 }
