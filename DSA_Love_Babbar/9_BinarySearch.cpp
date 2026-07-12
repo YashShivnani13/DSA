6f78bfbed81686d49398e5cc74665fe2a46ee305
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-07-12 12:13:35 +0530

    square root of a integer n

--- DSA_Love_Babbar/9_BinarySearch.cpp
+++ DSA_Love_Babbar/9_BinarySearch.cpp
@@ -118,6 +118,7 @@
 
 
 //Total no. of Occurence in sorted array
+
 // #include <iostream>
 // using namespace std;
 
@@ -189,30 +190,98 @@
 
 
 
+
+
+
+
 //pivot element (the smallest element) in a sorted and rotated array.
+
+// #include <iostream>
+// using namespace std;
+
+// int pivot(int arr[], int n){
+//     int s = 0;
+//     int e = n-1;
+//     int mid = s+(e-s)/2;
+
+//     while(s<e){
+//         if(arr[mid]>arr[0]){
+//             s = mid+1;
+//         }else{
+//             e=mid;
+//         }
+//         mid = s+(e-s)/2;
+//     }
+//     return s;
+// }
+
+// int main(){
+//     int arr [8] = {4,6,7,8,9,10,11,1};
+
+//     cout<<"Pivot is at " << pivot(arr,8) << endl;
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+//square root of a integer n
+
 #include <iostream>
 using namespace std;
 
-int pivot(int arr[], int n){
+long long int bs(int n) {
     int s = 0;
-    int e = n-1;
-    int mid = s+(e-s)/2;
+    int e = n;
+    long long int mid = s + (e - s) / 2;
+    long long int ans = -1;
 
-    while(s<e){
-        if(arr[mid]>arr[0]){
-            s = mid+1;
-        }else{
-            e=mid;
+    while (s <= e) {
+        long long int square = mid * mid;
+
+        if (square == n) {
+            return mid;
         }
-        mid = s+(e-s)/2;
+
+        if (square < n) {
+            ans = mid;
+            s = mid + 1;
+        } else {
+            e = mid - 1;
+        }
+
+        mid = s + (e - s) / 2;
     }
-    return s;
+
+    return ans;
+}
+
+double morePrecision(int n, int precision, int tempSol){
+    double factor = 1;
+    double ans = tempSol;
+
+    for(int i=0; i<precision; i++){
+        factor = factor/10;
+
+        for(double j = ans; j*j<n; j=j+factor){
+            ans = j;
+        }
+    }
+    return ans;
 }
 
 int main(){
-    int arr [8] = {4,6,7,8,9,10,11,1};
+    int n;
+    cout<<"Enter the number ";
+    cin>>n;
 
-    cout<<"Pivot is at " << pivot(arr,8) << endl;
+    int tempSol = bs(n);
+    cout<<" Answer is "<<morePrecision(n, 3, tempSol);
 
     return 0;
 }
