0d65a10c3a1fded2605b984cdcdaf3e78c5bacbe
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-07-14 19:54:21 +0530

    Selection sort, bubble sort

--- DSA_Love_Babbar/11_Sorting.cpp
+++ DSA_Love_Babbar/11_Sorting.cpp
@@ -1,28 +1,61 @@
 //SELECTION SORT
 
+// #include <iostream>
+// using namespace std;
+
+// void selectionSort(int arr[], int n)
+// {
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         int minIndex = i;
+//         for (int j = i + 1; j < n; j++)
+//         {
+//             if (arr[j] < arr[minIndex])
+//             {
+//                 minIndex = j;
+//             }
+//         }
+//         swap(arr[minIndex], arr[i]);
+//     }
+// }
+
+// int main()
+// {
+//     int arr[10] = {7, 9, 6, 8, 4, 5, 3, 2, 1, 0};
+//     selectionSort(arr, 10);
+//     for (int i = 0; i < 10; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     return 0;
+// }
+
+
+
+
+
+//BUBBLE SORT
+
+
 #include <iostream>
 using namespace std;
 
-void selectionSort(int arr[], int n)
-{
-    for (int i = 0; i < n - 1; i++)
-    {
-        int minIndex = i;
-        for (int j = i + 1; j < n; j++)
-        {
-            if (arr[j] < arr[minIndex])
-            {
-                minIndex = j;
+
+void bubbleSort(int arr[], int n)
+{   
+    for(int i=1; i<n; i++){   //0 se n-1 ya to 1 se n
+        for(int j=0; j<n-i; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j], arr[j+1]);
             }
         }
-        swap(arr[minIndex], arr[i]);
     }
 }
 
 int main()
 {
     int arr[10] = {7, 9, 6, 8, 4, 5, 3, 2, 1, 0};
-    selectionSort(arr, 10);
+    bubbleSort(arr, 10);
     for (int i = 0; i < 10; i++)
     {
         cout << arr[i] << " ";
