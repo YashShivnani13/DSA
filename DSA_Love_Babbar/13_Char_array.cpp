e5c9b7a53c42685a0808f0cb0e27c033e2855c33
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-07-30 16:16:49 +0530

    Char Array

---
+++ DSA_Love_Babbar/13_Char_array.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void reverse(char name[], int n){
+    int s = 0;
+    int e=n-1;
+
+    while(s<e){
+        swap(name[s++], name[e--]);
+    }
+
+}
+
+int getLength(char name[]){
+    int count=0;
+    for(int i=0; name[i]!='\0'; i++){
+        count++;
+    }
+    return count;
+}
+
+int main(){
+    char name[20];
+    cout<<"Enter name " ;
+    cin>> name;
+
+    cout<<"Your name is "<<name<<endl;
+    cout<<"length of name is "<< getLength(name)<<endl;;
+
+    int n = getLength(name);
+
+    reverse(name, n);
+    cout<<"reverse of name is "<< name<<endl;
+
+    return 0;
+
+}
