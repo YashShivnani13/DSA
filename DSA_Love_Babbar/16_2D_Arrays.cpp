8093eb3a49819edcb5c8ca50c8131b07a83719f1
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-08-03 17:24:41 +0530

    2D Array imp Questions

--- DSA_Love_Babbar/16_2D_Arrays.cpp
+++ DSA_Love_Babbar/16_2D_Arrays.cpp
@@ -47,7 +47,83 @@ int main(){
     int ans  = largestRowSum(arr, 3, 4);
 
     cout<<"Index of maximum sum row is " << ans;
-    
+
     
     return 0;
 }
+
+
+
+//Print Like A SINE Wave
+
+// #include <bits/stdc++.h> 
+// vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+// {
+//     vector <int> ans;
+//     for(int col=0; col<mCols; col++){
+
+//         if(col & 1){ //odd case
+//             for(int row = nRows-1; row>=0; row--){
+//                 ans.push_back(arr[row][col]);
+//             }
+//         }
+//         else{ //even case
+//             for(int row=0; row<nRows; row++){
+//                 ans.push_back(arr[row][col]);
+//             }
+//         }
+//     }
+//     return ans;
+// }
+
+
+
+
+
+//SPIRAL MATRIX
+
+// class Solution {
+// public:
+//     vector<int> spiralOrder(vector<vector<int>>& matrix) {
+
+//         vector<int> ans;
+//         int row = matrix.size();
+//         int col = matrix[0].size();
+//         int count = 0;
+//         int total = row*col;
+
+//         int startingRow = 0;
+//         int startingCol = 0;
+//         int endingRow = row-1;
+//         int endingCol = col-1;
+
+//         while(count<total){
+
+//             for(int i = startingCol; count<total && i<=endingCol; i++){
+//                 ans.push_back(matrix[startingRow][i]);
+//                 count++;
+//             }
+//             startingRow++;
+
+//             for(int i = startingRow ; count<total && i<=endingRow; i++){
+//                 ans.push_back(matrix[i][endingCol]);
+//                 count++;
+//             }
+//             endingCol--;
+
+//             for(int i = endingCol; count<total && i>= startingCol; i--){
+//                 ans.push_back(matrix[endingRow][i]);
+//                 count++;
+//             }
+//             endingRow--;
+
+//             for(int i = endingRow; count<total && i>=startingRow; i--){
+//                 ans.push_back(matrix[i][startingCol]);
+//                 count++;
+//             }
+//             startingCol++;
+
+//         }
+//         return ans;
+//     }
+// }; 
