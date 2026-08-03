931f3966d0123303c613b6e569e8f3c89ab509f4
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-08-03 15:12:56 +0530

    row wise and col wise sum

--- DSA_Love_Babbar/15_2D_Arrays.cpp
+++ DSA_Love_Babbar/15_2D_Arrays.cpp
@@ -71,10 +71,67 @@
 
 
 
-//Row wise sum
+//ROW WISE SUM
+
+// #include <iostream>
+// using namespace std;
+
+// void printSum(int arr[][4], int row, int col){
+//     cout<<"Printing sum " << endl;
+//     for(int row=0; row<3; row++){
+//         int sum=0;
+//         for(int col=0; col<4; col++){
+//             sum += arr[row][col];
+//         }
+//         cout<<sum<<endl;
+//     }
+//     cout << endl;
+// }
+
+
+// int main(){
+
+//     int arr[3][4];
+
+//     //taking input -> row wise input
+//     cout<<"Enter elements "<<endl;
+//     for(int i=0; i<3; i++){
+//         for(int j=0; j<4; j++){
+//             cin>>arr[i][j];
+//         }
+//     }
+
+//     // printing output
+
+//     for(int i=0; i<3; i++){
+//         for(int j=0; j<4; j++){
+//             cout << arr[i][j] << " ";
+//         }
+//         cout<<endl;
+//     }
+
+//     printSum(arr, 3, 4);
+//     return 0;
+// }
+
+
+
+//COLUMN WISE SUM
 
 #include <iostream>
 using namespace std;
+
+void printSum(int arr[][4], int row, int col){
+    cout<<"Printing sum col wise " << endl;
+    for(int col=0; col<4; col++){
+        int sum=0;
+        for(int row=0; row<3; row++){
+            sum += arr[row][col];
+        }
+        cout<<sum<<endl;
+    }
+    cout << endl;
+}
 
 
 int main(){
@@ -98,7 +155,6 @@ int main(){
         cout<<endl;
     }
 
-    
-
+    printSum(arr, 3, 4);
     return 0;
 }
