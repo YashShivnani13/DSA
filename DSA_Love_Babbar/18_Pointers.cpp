38e2ae9b33c9fe203523bb152cc0830b51bf8c41
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-08-09 15:32:00 +0530

    Updated Pointers

--- DSA_Love_Babbar/18_Pointers.cpp
+++ DSA_Love_Babbar/18_Pointers.cpp
@@ -8,21 +8,37 @@ int main(){
     //Address of operator -> &
     cout<<"The address of num is "<< &num <<endl;
 
+
+
     //pointer -> store address
 
 
-    //DECLARE POINTER
+    //DECLARE POINTER type 1
     int *ptr = &num;
     cout << *ptr << endl;  //gives value of variable whose address it is pointing
     cout << ptr << endl;   //gives address which is stored inside it
+    
+
+
+    //Pointer can update a value
+    *ptr = (*ptr)*(*ptr);
+    cout << "Updated value " << num << endl;
+
+
 
     //Size
     cout<<"Size of int is " << sizeof(num) << endl;
     cout<<"Size of pointer is " << sizeof(ptr) << endl;
 
+
+
+    //type 2 to initialize pointer
     double d = 12.20;
-    double *p1 = &d;
-    cout<<"Size of pointer is " << sizeof(p1) << endl;
+    double *p1 = 0;
+    p1 = &d;
+    cout<<"address stored in pointer is " << p1<< endl;
+    cout<<"value stored in pointer is " << *p1<< endl;
+    
 
     return 0;
 }
