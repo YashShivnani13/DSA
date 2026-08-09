8caceafcb782c9be6c899fa0739509de16f6d607
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-08-09 16:23:46 +0530

    Pointers Part 1

--- DSA_Love_Babbar/18_Pointers.cpp
+++ DSA_Love_Babbar/18_Pointers.cpp
@@ -17,6 +17,9 @@ int main(){
     int *ptr = &num;
     cout << *ptr << endl;  //gives value of variable whose address it is pointing
     cout << ptr << endl;   //gives address which is stored inside it
+
+    cout << &num << endl;  //gives address it is pointing
+    cout << num << endl;   //gives value
     
 
 
@@ -38,6 +41,26 @@ int main(){
     p1 = &d;
     cout<<"address stored in pointer is " << p1<< endl;
     cout<<"value stored in pointer is " << *p1<< endl;
+
+    
+
+    //copying a pointer
+    int *q = ptr;
+    cout << q << "-" << ptr << endl;
+    cout << *q << "-" << *ptr << endl;
+
+
+    
+    //playing with pointers
+    (*q)++;
+    cout<<*q<<" ";
+
+    *q = *q+1;
+    cout<<*q<<" ";
+
+    cout<<q<<" ";
+    q = q+1;      //address updates //as int stores 4 bytes so address is updated by 4 bytes
+    cout<<q<<" ";
     
 
     return 0;
