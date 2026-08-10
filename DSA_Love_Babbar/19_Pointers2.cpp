5d6e6e76e84b69ccc36f3940e905478435fb71d0
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-08-10 17:22:34 +0530

    POINTER IN ARRAY

--- DSA_Love_Babbar/19_Pointers2.cpp
+++ DSA_Love_Babbar/19_Pointers2.cpp
@@ -6,13 +6,47 @@ using namespace std;
 int main(){
     int arr[10] = {2,5,6};
 
-    cout<<" Address of first block of array is " << arr <<endl;
-    cout<<" Address of first block of array is " << &arr <<endl;
-    cout<<" Address of second block of array is " << &arr[1] <<endl;
+    // cout<<" Address of first block of array is " << arr <<endl;
+    // cout<<" Address of first block of array is " << &arr <<endl;
+    // cout<<" Address of second block of array is " << &arr[1] <<endl;
 
-    cout<<"Value at first index " << *arr << endl; 
-    cout<<"Update Value at first index " << *arr+1 << endl;
-    cout<<"Value at second index " << *(arr+1) << endl;
+    // cout<<"Value at first index " << *arr << endl; 
+    // cout<<"Update Value at first index " << *arr+1 << endl;
+    // cout<<"Value at second index " << *(arr+1) << endl;
+
+
+
+
+
+
+
+    // int temp[10];
+    // cout << sizeof(temp) <<endl;      //size of temp array
+    // cout << sizeof(*temp) <<endl;     //size of first value in temp array
+    // cout << sizeof(&temp) <<endl;     //size of address of first location
+
+    // cout<<endl;
+    // int *ptr = &temp[0];
+
+    // cout << sizeof(ptr) <<endl;     //prints address size i.e 8
+    // cout << sizeof(*ptr) <<endl;    //prints value size
+    // cout << sizeof(&ptr) <<endl;    //prints address size
+
+
+
+
+
+
+
+
+
+    
+    // int a[20] = {78,56,9,84};
+    // cout<< &a[0] <<endl;            //address of 0th index
+
+
+    // int *p = &a[0];
+    // cout<< &p <<endl;              //address of p
 
     return 0;
 }
