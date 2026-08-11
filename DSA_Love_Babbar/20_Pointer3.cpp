ef012d39f01ab762ce66aedc36b7df1a5349b29f
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-08-11 15:21:08 +0530

    POINTERS IN  CHARACTER ARRAY

---
+++ DSA_Love_Babbar/20_Pointer3.cpp
@@ -0,0 +1,15 @@
+//POINTERS IN  CHARACTER ARRAY
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[5] = {1,2,3,4,5};
+    cout << arr << endl;
+
+    char ch[6] = "abcde";
+    cout << ch << endl;
+
+    return 0;
+}
