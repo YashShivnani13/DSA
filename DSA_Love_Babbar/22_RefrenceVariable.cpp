ae6f194dd8ce9805acb1f97902144ec81328f17f
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-08-16 19:30:26 +0530

    Refrence Variable

--- DSA_Love_Babbar/22_RefrenceVariable.cpp
+++ DSA_Love_Babbar/22_RefrenceVariable.cpp
@@ -1,18 +1,40 @@
-//Refrence Variable
+//Reference Variable
 
 #include <iostream>
 using namespace std;
 
+//Return By Reference   ->   will give error (bad practice)
+// int& update3(int n){
+//     int a=10;
+//     int &ans = a;
+//     return ans;
+// }
+
+
+//Pass By Reference
+void update(int n){
+    n++;
+    cout<<n<<endl;
+}
+void update2(int &n){
+    n++;
+    cout<<n<<endl;
+}
+
 int main(){
     int i=5;
-    int &j = i;
+    // int &j = i;
 
-    cout << i << endl;
-    i++;
-    cout << i << endl;
-    j++;
-    cout << i << endl;
-    cout << j << endl;
+    // cout << i << endl;
+    // i++;
+    // cout << i << endl;
+    // j++;
+    // cout << i << endl;
+    // cout << j << endl;
+
+    update(i);
+    update2(i);
+    
 
     return 0;
 
