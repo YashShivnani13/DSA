d2c36c10c84a28828ba31c90115ee42b833fecf8
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-08-19 12:40:05 +0530

    Dyanmic 1D and 2D Array

--- DSA_Love_Babbar/23_dynamicArray.cpp
+++ DSA_Love_Babbar/23_dynamicArray.cpp
@@ -1,33 +1,84 @@
 //Dynamic Array
 
+
+
+//Creating 1-D array
+
+
+// #include <iostream>
+// using namespace std;
+
+// int getSum(int *arr, int n){
+
+//     int sum = 0;
+//     for(int i=0; i<n; i++){
+//         sum += arr[i];
+//     }
+//     return sum;
+// }
+
+// int main(){
+
+//     int n;
+//     cin>>n;
+
+//     //variable size array
+//     int *arr = new int[n];        
+
+
+//     //taking input
+//     for(int i=0; i<n; i++){
+//         cin >> arr[i];
+//     }
+
+//     int ans = getSum(arr, n);
+//     cout << ans;
+
+//     return 0;
+// }
+
+
+
+
+
+
+//Creating 2-D array
+
+
 #include <iostream>
 using namespace std;
 
-int getSum(int *arr, int n){
-
-    int sum = 0;
-    for(int i=0; i<n; i++){
-        sum += arr[i];
-    }
-    return sum;
-}
-
 int main(){
 
-    int n;
-    cin>>n;
+    int row;
+    cin>>row;
 
-    //variable size array
-    int *arr = new int[n];
+    int col;
+    cin>>col;
 
+    //Creation of dynamic 2D Array
 
-    //taking input
-    for(int i=0; i<n; i++){
-        cin >> arr[i];
+    int **arr = new int *[row];
+
+    for(int i=0; i<row; i++){
+        arr[i] = new int[col];
     }
 
-    int ans = getSum(arr, n);
-    cout << ans;
+    //taking input
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    //giving output
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout<<endl;
+    }
+
 
     return 0;
 }
