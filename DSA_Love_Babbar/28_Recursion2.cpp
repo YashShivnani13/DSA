b1db704ae078082af2dd8e4b79dc1cb94131b3c5
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-08-29 16:01:03 +0530

    Recursion

--- DSA_Love_Babbar/28_Recursion2.cpp
+++ DSA_Love_Babbar/28_Recursion2.cpp
@@ -125,6 +125,7 @@
 
 
 
+
 //Binary Search with Recursion
 
 
@@ -180,3 +181,212 @@
 
 //     return 0;
 // }
+
+
+
+
+
+
+
+//First and Last Position of an Element In Sorted Array using recursion
+
+
+// #include <iostream>
+// using namespace std;
+
+// int firstOcc(int arr[], int start, int end, int k) {
+
+//     // Base case
+//     if (start > end)
+//         return -1;
+
+//     int mid = start + (end - start) / 2;
+
+//     // If key is found
+//     if (arr[mid] == k) {
+//         int ans = firstOcc(arr, start, mid - 1, k);
+
+//         if (ans == -1)
+//             return mid;
+//         else
+//             return ans;
+//     }
+
+//     // Key is on right side
+//     else if (k > arr[mid]) {
+//         return firstOcc(arr, mid + 1, end, k);
+//     }
+
+//     // Key is on left side
+//     else {
+//         return firstOcc(arr, start, mid - 1, k);
+//     }
+// }
+
+
+// int lastOcc(int arr[], int start, int end, int k) {
+
+//     // Base case
+//     if (start > end)
+//         return -1;
+
+//     int mid = start + (end - start) / 2;
+
+//     // If key is found
+//     if (arr[mid] == k) {
+//         int ans = lastOcc(arr, mid + 1, end, k);
+
+//         if (ans == -1)
+//             return mid;
+//         else
+//             return ans;
+//     }
+
+//     // Key is on right side
+//     else if (k > arr[mid]) {
+//         return lastOcc(arr, mid + 1, end, k);
+//     }
+
+//     // Key is on left side
+//     else {
+//         return lastOcc(arr, start, mid - 1, k);
+//     }
+// }
+
+
+// int main() {
+
+//     int arr[10] = {1,2,3,3,3,3,4,6,7,9};
+
+//     int First = firstOcc(arr, 0, 9, 3);
+//     int Last = lastOcc(arr, 0, 9, 3);
+
+//     cout << "First occurrence of key is " << First << endl;
+//     cout << "Last occurrence of key is " << Last << endl;
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+//Total no. of Occurence in sorted array using recursion
+
+
+// #include <iostream>
+// using namespace std;
+
+// int firstOcc(int arr[], int start, int end, int k) {
+
+//     // Base case
+//     if (start > end)
+//         return -1;
+
+//     int mid = start + (end - start) / 2;
+
+//     if (arr[mid] == k) {
+
+//         int ans = firstOcc(arr, start, mid - 1, k);
+
+//         if (ans == -1)
+//             return mid;
+//         else
+//             return ans;
+//     }
+
+//     else if (k > arr[mid]) {
+//         return firstOcc(arr, mid + 1, end, k);
+//     }
+
+//     else {
+//         return firstOcc(arr, start, mid - 1, k);
+//     }
+// }
+
+
+// int lastOcc(int arr[], int start, int end, int k) {
+
+//     // Base case
+//     if (start > end)
+//         return -1;
+
+//     int mid = start + (end - start) / 2;
+
+//     if (arr[mid] == k) {
+
+//         int ans = lastOcc(arr, mid + 1, end, k);
+
+//         if (ans == -1)
+//             return mid;
+//         else
+//             return ans;
+//     }
+
+//     else if (k > arr[mid]) {
+//         return lastOcc(arr, mid + 1, end, k);
+//     }
+
+//     else {
+//         return lastOcc(arr, start, mid - 1, k);
+//     }
+// }
+
+
+// int main() {
+
+//     int arr[10] = {1,2,3,3,3,3,4,6,7,9};
+
+//     int First = firstOcc(arr, 0, 9, 3);
+//     int Last = lastOcc(arr, 0, 9, 3);
+
+//     int total = (Last - First) + 1;
+
+//     cout << "Total no. of occurrences: " << total << endl;
+
+//     return 0;
+// }
+
+
+
+
+
+
+//pivot element (the smallest element) in a sorted and rotated array with recursion
+
+
+
+#include <iostream>
+using namespace std;
+
+int pivot(int arr[], int s, int e) {
+
+    // Base case
+    if (s == e)
+        return s;
+
+    int mid = s + (e - s) / 2;
+
+    if (arr[mid] > arr[0]) {
+        // Pivot is on the right
+        return pivot(arr, mid + 1, e);
+    }
+    else {
+        // Pivot is on the left (including mid)
+        return pivot(arr, s, mid);
+    }
+}
+
+
+int main() {
+
+    int arr[8] = {4, 6, 7, 8, 9, 10, 11, 1};
+
+    cout << "Pivot is at " << pivot(arr, 0, 7) << endl;
+
+    return 0;
+}
+
