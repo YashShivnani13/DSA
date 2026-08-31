54fbda056e178a6224ade59dfc007082a55f9219
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-08-31 15:16:55 +0530

    Recursion

--- DSA_Love_Babbar/29_Recursion3.cpp
+++ DSA_Love_Babbar/29_Recursion3.cpp
@@ -1,34 +1,80 @@
 //reverse string using recursion
 
-#include <iostream>
-using namespace std;
 
-void reverse(string& str, int i){
+// #include <iostream>
+// using namespace std;
 
-    int j = str.size()-i-1;
+// void reverse(string& str, int i){
+
+//     int j = str.size()-i-1;
+
+//     //base case
+//     if(i>j){
+//         return;
+//     }
+
+//     //processing
+//     swap(str[i], str[j]);
+//     i++;
+//     j--;
+
+//     //recursion
+//     reverse(str, i);
+// }
+
+// int main(){
+
+//     string strs = "yash";
     
-    //base case
-    if(i>j){
-        return;
-    }
+//     reverse(strs, 0);
 
-    //processing
-    swap(str[i], str[j]);
-    i++;
-    j--;
+//     cout << strs << endl;
 
-    //recursion
-    reverse(str, i);
-}
+//     return 0;
 
-int main(){
+// }
 
-    string strs = "yash";
-    
-    reverse(strs, 0);
 
-    cout << strs << endl;
 
-    return 0;
 
-}
+
+
+
+
+//Check Palindrome using recursion
+
+
+// #include <iostream>
+// using namespace std;
+
+// bool check(string s, int i, int j){
+
+//     //base case
+//     if(i>j){
+//         return true;
+//     }
+
+//     if(s[i] != s[j]){
+//         return false;
+//     }
+//     else{
+//         //recursive call
+//         return check(s, i+1, j-1);
+//     }
+// }
+
+// int main(){
+//     string str = "aabaa";
+//     cout<<endl;
+
+//     bool isPalindrome = check(str, 0, str.size()-1);
+
+//     if(isPalindrome){
+//         cout << "It is a Palindrome" << endl;
+//     }
+//     else{
+//         cout << "It is not a Palindrome" << endl;
+//     }
+
+//     return 0;
+// }
