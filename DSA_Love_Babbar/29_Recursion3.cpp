8e0765882fed926e0cd9d829d59fa49a8aefb79d
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-08-31 20:40:48 +0530

    Recursion

--- DSA_Love_Babbar/29_Recursion3.cpp
+++ DSA_Love_Babbar/29_Recursion3.cpp
@@ -78,3 +78,49 @@
 
 //     return 0;
 // }
+
+
+
+
+
+
+
+
+//Power using recursion
+
+
+#include <iostream>
+using namespace std;
+
+int power(int a, int b){
+
+    //base case
+    if(b==0)
+        return 1;
+    if(b==1)
+        return a;
+
+    //recursive call
+    int ans = power(a, b/2);
+
+    //b is even
+    if(b%2 == 0){
+        return ans * ans;
+    }
+    else{
+        //b is odd
+        return a*ans*ans;
+    }
+
+}
+
+int main(){
+
+    int a,b;
+    cin>>a>>b;
+
+    int ans = power(a,b);
+    cout << "Answer is " << ans;
+
+    return 0;
+}
