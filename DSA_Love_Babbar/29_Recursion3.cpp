ce50bda0ddbce3a587d02df4d21a990270616ab4
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-09-02 15:04:44 +0530

    Sorting using Recursion

--- DSA_Love_Babbar/29_Recursion3.cpp
+++ DSA_Love_Babbar/29_Recursion3.cpp
@@ -134,38 +134,130 @@
 //Bubble sort using recursion
 
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-void bubbleSort(int *arr, int size){
+// void bubbleSort(int *arr, int size){
 
-    //base case - already sorted
-    if(size == 0 || size == 1){
-        return;
-    }
+//     //base case - already sorted
+//     if(size == 0 || size == 1){
+//         return;
+//     }
 
-    //processing 
-    for(int i=0; i<size-1; i++){
-        if(arr[i] > arr[i+1]){
-            swap(arr[i], arr[i+1]);
-        }
-    }
+//     //processing 
+//     for(int i=0; i<size-1; i++){
+//         if(arr[i] > arr[i+1]){
+//             swap(arr[i], arr[i+1]);
+//         }
+//     }
 
-    //recursive call
-    bubbleSort(arr, size-1);
+//     //recursive call
+//     bubbleSort(arr, size-1);
 
     
-}
+// }
 
-int main(){
-    int arr[5] = {4,6,1,9,2};
+// int main(){
+//     int arr[5] = {4,6,1,9,2};
 
 
-    bubbleSort(arr, 5);
+//     bubbleSort(arr, 5);
 
-    for(int i=0; i<5; i++){
-        cout<< arr[i] <<" ";
-    }
+//     for(int i=0; i<5; i++){
+//         cout<< arr[i] <<" ";
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
+
+
+
+//Selection sort using recursion
+
+
+// #include <iostream>
+// using namespace std;
+
+// void SelectionSort(int *arr, int n){
+
+//     //base case
+//     if(n == 0 || n == 1){
+//         return;
+//     }
+
+//     //processing
+//     for(int i=0; i<n-1; i++){
+//         int minIndex = i;
+//         for(int j=i+1; j<n; j++){
+//             if(arr[j] < arr[minIndex]){
+//                 minIndex = j;
+//             }
+//         }
+//         swap(arr[minIndex], arr[i]);
+//     }
+
+//     //recursive call
+//     SelectionSort(arr, n-1);
+// }
+
+// int main(){
+//     int arr[5] = {4,6,1,9,2};
+
+
+//     SelectionSort(arr, 5);
+
+//     for(int i=0; i<5; i++){
+//         cout<< arr[i] <<" ";
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+//Insertion sort using recursion
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// void InsertionSort(int *arr, int n){
+
+//     // Base case
+//     if(n <= 1){
+//         return;
+//     }
+
+//     // Sort first n-1 elements
+//     InsertionSort(arr, n-1);
+
+//     // Insert the last element into its correct position
+//     int last = arr[n-1];
+//     int j = n-2;
+
+//     while(j >= 0 && arr[j] > last){
+//         arr[j+1] = arr[j];
+//         j--;
+//     }
+
+//     arr[j+1] = last;
+// }
+
+// int main(){
+
+//     int arr[5] = {4, 6, 1, 9, 2};
+
+//     InsertionSort(arr, 5);
+
+//     for(int i = 0; i < 5; i++){
+//         cout << arr[i] << " ";
+//     }
+
+//     return 0;
+// }
