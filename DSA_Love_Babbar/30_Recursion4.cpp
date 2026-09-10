d4791d5ee701a288564087380831891e35b668ee
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-09-10 17:09:42 +0530

    RAT IN A MAZE

--- DSA_Love_Babbar/30_Recursion4.cpp
+++ DSA_Love_Babbar/30_Recursion4.cpp
@@ -305,96 +305,101 @@
 //RAT IN A MAZE PROBLEM
 
 
-// class Solution {
-//   private:
-//     bool issafe(int x, int y, int n, vector<vector<int>> visited, vector<vector<int>>& m){
-        
-//         if((x>=0 && x<n) && (y>=0 && y<n) && visited[x][y]=0 && m[x][y] =1){
-//             return true;
-//         } 
-//         else{
-//             return false;
-//         }
+// #include <bits/stdc++.h> 
+
+// bool check(vector<vector<int>>& arr, int n, int x, int y,
+//            vector<vector<int>>& visited){
+//     if((x>=0 && x<n) && (y>=0 && y<n) && arr[x][y]==1 && visited[x][y]==0){
+//         return true;
 //     }
-    
-//     void solve(vector<vector<int>>& m ,int n, vector<string> &ans, int x, int y, vector<vector<int>> visited, string path){
-        
-//         //base case
-//         if(x == n-1 && y == n-1){
-//             ans.push_back(path);
-//             return;
-//         }
-        
-//         visited [x][y] = 1;
-        
-//         //4 choices up/down/right/left
-        
-//         //down
-//         int newx = x+1;
-//         int newy = y;
-//         if(isSafe(newx,newy,n,visited,m)){
-//             path.push_back('D');
-//             solve(m,n,ans,newx,newy,visited,path);
-//             path.pop_back();
-//         }
-        
-//         //up
-//         newx = x-1;
-//         newy = y;
-//         if(isSafe(newx,newy,n,visited,m)){
-//             path.push_back('U');
-//             solve(m,n,ans,newx,newy,visited,path);
-//             path.pop_back();
-//         }
-        
-//         //left
-//         newx = x;
-//         newy = y-1;
-//         if(isSafe(newx,newy,n,visited,m)){
-//             path.push_back('L');
-//             solve(m,n,ans,newx,newy,visited,path);
-//             path.pop_back();
-//         }
-        
-//         //right
-//         newx = x;
-//         newy = y+1;
-//         if(isSafe(newx,newy,n,visited,m)){
-//             path.push_back('R');
-//             solve(m,n,ans,newx,newy,visited,path);
-//             path.pop_back();
-//         }
-        
-//         visited[x][y] = 0;
+//     else{
+//         return false;
+//     }
+// }
+
+// void solve(vector < vector < int >> & arr, int n, vector<string>& ans, string path, vector<vector<int>>& visited, int x, int y){
+
+//     //base case
+//     if(x == n-1 && y == n-1){
+//         ans.push_back(path);
+//         return;
 //     }
 
-//   public:
-//     vector<string> ratInMaze(vector<vector<int>>& m) {
+
+//     visited[x][y]=1;
+
+
+
+//     //left
+//     int newx = x;
+//     int newy = y-1;
+
+//     if(check(arr,n,newx,newy,visited)){
+//         path.push_back('L');
+//         solve(arr,n,ans,path,visited,newx,newy);
+//         path.pop_back();
+//     }
+
+
+//     //right
+//     newx = x;
+//     newy = y+1;
+
+//     if(check(arr,n,newx,newy,visited)){
+//         path.push_back('R');
+//         solve(arr,n,ans,path,visited,newx,newy);
+//         path.pop_back();
+//     }
+
+//     //down
+//     newx = x+1;
+//     newy = y;
+
+//     if(check(arr,n,newx,newy,visited)){
+//         path.push_back('D');
+//         solve(arr,n,ans,path,visited,newx,newy);
+//         path.pop_back();
+//     }
+
+//     //up
+//     newx = x-1;
+//     newy = y;
+
+//     if(check(arr,n,newx,newy,visited)){
+//         path.push_back('U');
+//         solve(arr,n,ans,path,visited,newx,newy);
+//         path.pop_back();
+//     }
+
+
+//     visited[x][y] = 0;
+// }
+
+
+// vector < string > searchMaze(vector < vector < int >> & arr, int n) {
 //     vector<string> ans;
-//     int n = m.size();
 
-//         // base case
-//         if(m[0][0] == 0){
-//             return 0;
-//         }
-        
-//         int srcx = 0;
-//         int srcy = 0;
-        
-//         vector<vector<int>> visited = m;
-        
-//         //initialize with 0
-//         for(int i=0; i<n; i++){
-//             for(int j=0; j<n; j++){
-//                 visited[i][j] = 0;
-//             }
-//         }
-        
-//         string path = "";
-        
-//         solve(m,n,ans,srcx,srcy,visited,path);
-//         sort(ans.begin(), ans.end());
-//         return ans;
-        
+
+//     //base case
+//     if(arr[0][0]==0){
+//         return {};
 //     }
-// };
+
+//     int srcx = 0;
+//     int srcy = 0;
+
+//     vector < vector < int >> visited = arr;
+
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<n; j++){
+//             visited[i][j]=0;
+//         }
+//     }
+
+//     string path = "";
+
+//     solve(arr,n,ans,path,visited,srcx,srcy);
+
+//     sort(ans.begin(), ans.end());
+//     return ans;
+// }
