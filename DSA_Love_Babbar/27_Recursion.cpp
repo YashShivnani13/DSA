6643588a8566c36b0a5d375b42444a0ca268515d
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-09-11 16:53:00 +0530

    recursion

--- DSA_Love_Babbar/27_Recursion.cpp
+++ DSA_Love_Babbar/27_Recursion.cpp
@@ -134,3 +134,30 @@ int main(){
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+// //FIBBONACCI NUMBER
+
+
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if(n==0){
+//             return 0;
+//         }
+//         if(n==1){
+//             return 1;
+//         }
+
+//         int a = fib(n-1)+fib(n-2);
+
+//         return a;
+//     }
+// };
