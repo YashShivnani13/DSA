87bc1224ea78e6f1c012081c7e9dd667e86c156d
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-09-12 18:40:18 +0530

    OOPs basics

---
+++ DSA_Love_Babbar/31_OOPs.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class hero{
+
+    //properties
+    int health;
+};
+
+
+int main(){
+
+    //create object
+    hero h1;
+
+    cout << sizeof(h1);
+
+    return 0;
+}
