096dc8d4bfd4ea7a40acf5ca948690445951c631
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-09-13 17:31:56 +0530

    OOPs basics

--- DSA_Love_Babbar/31_OOPs.cpp
+++ DSA_Love_Babbar/31_OOPs.cpp
@@ -1,42 +1,51 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-class hero{
+// class hero{
 
-    //properties
-    public:  //access modifiers
-    int health;
+//     //properties
+//     public:  //access modifiers
+//     int health;
 
-    char getname(){           //getter
-        return name;
-    }
+//     char getname(){           //getter
+//         return name;
+//     }
 
-    void setname(char n){     //setter
-        name = n;
-    }
+//     void setname(char n){     //setter
+//         name = n;
+//     }
 
-    private:
-    char name;
+//     private:
+//     char name;
 
     
-};
+// };
 
-int main(){
+// int main(){
 
-    //create object          //static allocation         
-    hero h1;
-
-    //dynamic allocation
-    hero *h2 = new hero;
-
-    //access class properties
-    h1.health = 80;
-    
-    h1.setname('A');
     
 
-    cout << "name is " << h1.getname() << endl;
+//     //create object          //static allocation         
+//     hero h1;
+
+//     //access class properties
+//     h1.health = 80;
+    
+//     cout<<"health is " << h1.health <<endl; 
+
+//     //dynamic allocation
+//     hero *h2 = new hero;
+
+//     //access class properties
+//     (*h2).health = 90;
+
+//     cout<<"health is " << (*h2).health <<endl;
+//     cout<<"health is " << h2->health <<endl;
+
+    
+//     h1.setname('A');
+//     cout << "name is " << h1.getname() << endl;
 
 
-    return 0;
-}
+//     return 0;
+// }
