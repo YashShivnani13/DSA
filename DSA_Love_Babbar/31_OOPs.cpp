d146e11fd60b261a929109ef7fca76c96d05f359
Yash_Shivnani <shivnaniyash2@gmail.com>
2026-09-13 17:18:17 +0530

    oops

--- DSA_Love_Babbar/31_OOPs.cpp
+++ DSA_Love_Babbar/31_OOPs.cpp
@@ -4,16 +4,39 @@ using namespace std;
 class hero{
 
     //properties
+    public:  //access modifiers
     int health;
-};
 
+    char getname(){           //getter
+        return name;
+    }
+
+    void setname(char n){     //setter
+        name = n;
+    }
+
+    private:
+    char name;
+
+    
+};
 
 int main(){
 
-    //create object
+    //create object          //static allocation         
     hero h1;
 
-    cout << sizeof(h1);
+    //dynamic allocation
+    hero *h2 = new hero;
+
+    //access class properties
+    h1.health = 80;
+    
+    h1.setname('A');
+    
+
+    cout << "name is " << h1.getname() << endl;
+
 
     return 0;
 }
